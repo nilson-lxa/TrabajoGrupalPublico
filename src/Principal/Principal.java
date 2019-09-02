449030c9c9ecb20e9ef30b30387508d9237748d8
ALUMNO - FIIS <ALUMNO - FIIS@DESKTOP-6MPVBL2>
2019-09-02 10:19:59 -0500

    Trabajo grupal publico

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Principal;
 
-/**
- *
- * @author ALUMNO - FIIS
- */
 public class Principal {
-    
+    public static void main(String[] args) {
+        System.out.println("Memos esta en otras :v");
+    }
 }
