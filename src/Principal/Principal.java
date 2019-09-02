37185e64bacceed99fc79bad275ae78567273996
nilson-lxa <54809119+nilson-lxa@users.noreply.github.com>
2019-09-02 10:20:53 -0500

    Update Principal.java

--- src/Principal/Principal.java
+++ src/Principal/Principal.java
@@ -1,7 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package Principal;
 
+/**
+ *
+ * @author ALUMNO - FIIS
+ */
 public class Principal {
-    public static void main(String[] args) {
-        System.out.println("Memos esta en otras :v");
-    }
+    System.out.print("Hola Iama");
 }
